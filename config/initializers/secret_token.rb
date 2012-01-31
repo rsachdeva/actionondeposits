a9e4423cf1cde11d7d65a12513dca931651a7e0c
Rohit Sachdeva <growth@drinnovations.us>
2012-01-31 15:47:08 -0600

    [0.5v] Initial rails 3.2 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecatalog::Application.config.secret_token = '6430e03cc1e73767d5b624e7a185f7bd57532e085b686e9612fff1581dac518a3a7100b62d2e4360a580ce75f41617c8c7fd1d995d4fa7997657845d0c883ea6'
