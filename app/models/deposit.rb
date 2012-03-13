397cdbade3a1fca89cc2aafb495f01c1a327798e
Rohit Sachdeva <growth@drinnovations.us>
2012-03-13 14:24:14 -0500

    [0.5v] Deposit with initial expiry methods

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -2,6 +2,8 @@ class Deposit
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  BEFORE_MAX = 30.freeze
+
   field :bank_name, :type => String
   field :account_number, :type => String
   field :phone_number, :type => String
@@ -13,4 +15,23 @@ class Deposit
   field :apy, :type => BigDecimal
   field :start_date, :type => Date
   field :comment, :type => String
+  field :confirmation_taken, :type => Boolean, :default => false
+
+  # when confirm asked then start_date should be moving target and hence should be updated
+  def under_expiry_alert_period?
+    (Date.today > deadline_for(BEFORE_MAX) ) && !self.confirmation_taken
+  end
+
+  def deadline_for(before)
+    expiry_date - before.days
+  end
+
+  def expiry_date
+    self.start_date + self.term_in_days.days
+  end
+
+  def self.count_under_expiry_alert_period
+    Deposit.all.map {|d| d.under_expiry_alert_period? }.reject{ |to_alert| !to_alert}.length
+  end
+
 end
